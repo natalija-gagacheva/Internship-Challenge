420f4f41db9b41e33df0dfa33f61c4535394d7ac
Natalija Gagacheva <164746782+natalija-gagacheva@users.noreply.github.com>
2025-02-12 17:17:30 +0100

    Add corrected Python code

--- TeamLift-Internship-Challenge/app.py
+++ TeamLift-Internship-Challenge/app.py
@@ -1,55 +1,37 @@
-#Importing Flask so we can be able to create web applications that accept HTTP requests
 from flask import Flask, request, jsonify
-from difflib import get_close_matches
 
-#We create a web app instance where this script is the main app.
 app = Flask(__name__)
 
-ADMIN_SKILLS = ['Python', 'relational database', 'Software engineering', 'data science', 'NLP',
-                'natural language processing']
+adminSkills = ['Python', 'relational database', 'Software engineering', 'data science', 'NLP', 'natural language processing']
 
+#Implementing match method
+def matchMethod(userSkill):
+    return [skill for skill in adminSkills if skill == userSkill]
 
-#Implementing match methods
-def exact_match(userSkill):
-    return [skill for skill in ADMIN_SKILLS if skill.lower() == userSkill.lower()]
-
-
-def partial_match(userSkill):
-    return get_close_matches(userSkill, ADMIN_SKILLS)
-
-#Routing
 @app.route('/')
 def home():
-    return "Flask API is running!"
+    return "Connection is successful"
 
 #this method only accepts post requests submitted by the client
-@app.route('/match', methods=['POST'])
-def match_skill():
+@app.route('/match-methods', methods=['POST'])
+def matchingMethods():
     #extracting data sent by the user
     userData = request.json
     #extracting the specific skill sent by user
-    userSkill = userData.get('skill_name')
+    userSkill = userData.get('skill')
 
-    #checks if the user skill exists. Throws error if doesn't exist
-    if not userSkill:
-        return jsonify({'error': 'Skill name is required'}), 400
+    matchResult = matchMethod(userSkill)
 
-    #running the match methods
-    exactMatchResults = exact_match(userSkill)
-    partialMatchResults = partial_match(userSkill)
-
-    #creating a response dictionary that stores the skill submitted by the user
-    #and results from both methods
-    responseDictionary = {
+    # creating a dictionary that stores the skill submitted by the user and results from both methods
+    dict = {
         'user input': userSkill,
-        'exact matches': exactMatchResults,
-        'partial matches': partialMatchResults
+        'exact matches': matchResult,
     }
 
-    #converts the response into a JSON file so the client will be able
-    #to recieve the result. JSON is the standard format for web APIs and is easily processed by web apps
-    return jsonify(responseDictionary)
+    # convert the response into a JSON file so the client will be able to recieve the result.
+    return jsonify(dict)
 
 #this part of the code starts the Flask web server
 if __name__ == '__main__':
     app.run(debug=True)
+
